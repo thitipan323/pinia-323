8391addc743cc49f0621f4a0bf4c0ffd72a5af54
thitipan323 <akitokung71@gmail.com>
2023-10-06 21:58:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <navbar_product />
+  <div class="container">
+    <router-view />
+  </div>
+
+  <h1 >รายการสินค้าตัวอย่างที่สมมุติทุกอย่างไม่มีอันไหนของจริง</h1>
+  
+<img src="https://th.bing.com/th/id/R.ee87e6dfbc3c654194896d509d637412?rik=eTnr0C8PHdHLaA&riu=http%3a%2f%2fbouassa.org%2fen%2fwp-content%2fuploads%2f2016%2f03%2fneedyou.jpg&ehk=Sv%2fl22AmohmztaNb2WPwLj29HFuCqBvaGfcqGk6Xz1c%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="">
+</template>
+
+
+<script setup>
+import { onMounted } from 'vue'
+import navbar_product from './components/navbar_product.vue';
+
+// useProductStore
+import { useProductStore } from './store/product'
+const product_store = useProductStore()
+
+//useCartStore
+import { useCartStore } from './store/cart';
+const cart_store = useCartStore()
+
+
+onMounted(() => product_store.fetch_products())
+onMounted(() => cart_store.loadFromLocalStorage())
+
+
+
+</script>
+
+<style lang="scss" scoped>
+h1{
+  text-align: center;
+}
+
+img{
+  max-width: 540px;
+  min-height: 540px;
+  margin-left: 35rem;
+}
+</style>
---
+++ src/components/navbar_product.vue
@@ -0,0 +1,39 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-warning p-3" data-bs-theme="dark"  >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Shoping
+    </a>
+    <img src="https://wallpaperaccess.com/full/168399.jpg" alt="">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <routerLink to="/" class="nav-link" aria-current="page">Home </routerLink>
+        </li>
+        <li class="nav-item">
+          <routerLink to="/cart" class="nav-link" aria-current="page">สินค้า</routerLink>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+</template>
+
+<script setup>
+import { useProductStore } from '../store/product';
+
+const product_store = useProductStore()
+
+</script>
+
+<style lang="scss" scoped>
+
+img{
+  max-width: 60px;
+  max-height: 60px;
+}
+
+</style>
